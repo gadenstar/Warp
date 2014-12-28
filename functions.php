99ca8e91e2d9a9f571efc3bec7da25ce51d97340
TanX <3510926@qq.com>
2014-12-28 10:50:47 +0800

    添加uikit JS

--- functions.php
+++ functions.php
@@ -105,9 +105,9 @@ function warp_scripts() {
 
 	wp_enqueue_style( 'warp-style', get_stylesheet_uri() );
 
-	wp_enqueue_script( 'warp-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
+	wp_enqueue_script( "jquery" );
 
-	wp_enqueue_script( 'warp-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
+	wp_enqueue_script( 'uikit', get_template_directory_uri() . '/inc/js/uikit.min.js', array(), '20120206');
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
